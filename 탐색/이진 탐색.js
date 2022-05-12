fa9b0ec70bd909e9a0901642ada75afa1e1dd0b8
JongHoonSon <roka1684@gmail.com>
2022-05-12 22:33:39 +0900

    이진 탐색 코드로 구현

--- 탐색/이진 탐색.js
+++ 탐색/이진 탐색.js
@@ -23,3 +23,37 @@
 // 위 과정을 반복함
 
 // 시간복잡도 : 단계마다 탐색 범위를 2로 나누기 때문에  => O(log N)
+
+// 구현 방식
+
+let array = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18];
+let target = 4;
+
+let time = 0;
+
+function binary_search(start, end) {
+  while (start <= end) {
+    time++;
+    let mid = Math.floor((start + end) / 2);
+    if (array[mid] === target) {
+      return mid;
+    } else if (array[mid] > target) {
+      end = mid - 1;
+    } else if (array[mid] < target) {
+      start = mid + 1;
+    }
+  }
+  return false;
+}
+
+let result = binary_search(0, array.length - 1);
+
+if (result === false) {
+  console.log(
+    `배열에 원소 ${target}가(이) 존재하지 않습니다. (탐색 횟수 : ${time}회)`
+  );
+} else {
+  console.log(
+    `배열 상에서 원소 ${target}의 인덱스는 ${result} 입니다. (탐색 횟수 : ${time}회)`
+  );
+}
